66cf34369bb1ce9288e13060be237d1069c48ccc
Jhonny Mata <solucionesmatajm@gmail.com>
2016-11-23 13:44:34 -0400

    parseo de fechas para evitar errores en motores de base de datos

--- src/BackEnd/SoulHand/Academic/People.php
+++ src/BackEnd/SoulHand/Academic/People.php
@@ -9,14 +9,15 @@ class People{
 		$this->database=$pdo;
 	}
 	public function create($input){
+		$datetime = \DateTime::createFromFormat('d-m-Y', $input["birthdate"]);
 		$data=[
 			"persona_cedula"=>$input["dni"],
 			"persona_nombre"=>$input["first_name"],
 			"persona_apellido"=>$input["last_name"],
 			"persona_telefono"=>$input["phone"],
-			"persona_fecha_nacimiento"=>$input["birthdate"],
+			"persona_fecha_nacimiento"=>$datetime->format('Y-m-d'),
 			"persona_correo"=>$input["email"]
-		];		
+		];
 		$stm=$this->database->prepare("INSERT INTO persona (
 			persona_cedula,
 			persona_nombre,
@@ -37,12 +38,12 @@ class People{
 		)
 		");
 		$stm->bindValue(':persona_imagen', (isset($input["image"])) ? $input["image"] : NULL, PDO::PARAM_LOB);
-		$stm->bindValue(':persona_cedula', $input["dni"], PDO::PARAM_STR);
-		$stm->bindValue(':persona_nombre', $input["first_name"], PDO::PARAM_STR);
-		$stm->bindValue(':persona_apellido', $input["last_name"], PDO::PARAM_STR);
-		$stm->bindValue(':persona_telefono', $input["phone"], PDO::PARAM_STR);
-		$stm->bindValue(':persona_fecha_nacimiento', $input["birthdate"], PDO::PARAM_STR);
-		$stm->bindValue(':persona_correo', $input["email"], PDO::PARAM_STR);
+		$stm->bindValue(':persona_cedula', $data["persona_cedula"], PDO::PARAM_STR);
+		$stm->bindValue(':persona_nombre', $data["persona_nombre"], PDO::PARAM_STR);
+		$stm->bindValue(':persona_apellido', $data["persona_apellido"], PDO::PARAM_STR);
+		$stm->bindValue(':persona_telefono', $data["persona_telefono"], PDO::PARAM_STR);
+		$stm->bindValue(':persona_fecha_nacimiento', $data["persona_fecha_nacimiento"], PDO::PARAM_STR);
+		$stm->bindValue(':persona_correo', $data["persona_correo"], PDO::PARAM_STR);
 		$query=$stm->execute();
 		if(!$query){
 			throw new PDOException("Ya existe la persona!");			
