635fc0e8d643ce10c163ecee16b2752e48e70010
Jhonny Mata <solucionesmatajm@gmail.com>
2017-02-13 13:09:09 -0400

    Pruebas de redundancia

--- Test/CRUD.spec.js
+++ Test/CRUD.spec.js
@@ -1,3 +1,5 @@
+var InsertException=require("../src/BackEnd/SoulHand/Exceptions/InsertException.js");
+var VoidException=require("../src/BackEnd/SoulHand/Exceptions/VoidException.js");
 var db=require("./prepare.js");
 var faker = require('faker');
 var Grade=require("../src/BackEnd/SoulHand/CRUD.js");
@@ -28,6 +30,15 @@ describe("CRUD clase Table Grades",function(){
 			done();
 		});
 	});
+	it("Create duplicated Element Grade",function(done){
+		var input={
+			name:find.name
+		};
+		grade.add(input,input).catch(function(error){
+			expect(error instanceof InsertException).toEqual(true);
+			done();
+		});
+	});
 	it("Delete Element Grade",function(done){
 		grade.remove({_id:find._id}).then(function(data){
 			done();
@@ -37,22 +48,47 @@ describe("CRUD clase Table Grades",function(){
 			//fail(error.toString());
 		});
 	});
+	it("Delete not exist Element Grade",function(done){
+		var id="58a1c1a27093140970ad3751";
+		grade.remove({_id:id}).catch(function(error){
+			expect(error instanceof VoidException).toEqual(true);					
+			done();
+		});
+	});
 	it("Update Element Grade",function(done){
+		var update=faker.name.findName();
+		grade.update({_id:find._id},function(info){
+			info.name=update;
+			return info;
+		}).then(function(data){
+			expect(update.toUpperCase()).toBe(data.name);
+			done();
+		});
+	});
+	it("Update not exist Element Grade",function(done){
+		var id="58a1c1a27093140970ad3751";
+		var update=faker.name.findName();
+		grade.update({_id:id},function(info){
+			info.name=update;
+			return info;
+		}).catch(function(error){
+			console.log(error.toString())
+			expect(error instanceof VoidException).toEqual(true);					
+			done();
+		});
+	});
+	it("find Element Grade",function(done){
 		var input={
 			name:faker.name.findName()
 		};
 		grade.add(input,input).then(function(data){
-			var update=faker.name.findName();
-			grade.update({_id:data._id},function(info){
-				info.name=update;
-				return info;
-			}).then(function(data){
-				expect(update.toUpperCase()).toBe(data.name);
-				done();
-			});
+			return grade.find({_id:data._id});
+		}).then(function(data){
+			expect(input.name.toUpperCase()).toBe(data.name);
+			done();
 		});
 	});
-	it("find Element Grade",function(done){
+	it("find not exist Element Grade",function(done){
 		var input={
 			name:faker.name.findName()
 		};
