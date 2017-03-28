4ff0254495120052c3f66a68b9d849ed5e0da9b1
Xiu-ling Rodriguez <kiralicius@gmail.com>
2017-03-27 23:24:27 -0400

    nuevas correcciones

--- src/FrontEnd/main.tsx
+++ src/FrontEnd/main.tsx
@@ -12,6 +12,11 @@ import {PageIndex} from './templates/PageIndex';
 import {Login} from './templates/Login';
 import {PageTeacher} from './templates/PageTeacher';
 import {PageTeacherCreate} from './templates/PageTeacherCreate';
+import {PageStudent} from './templates/PageStudent';
+import {PageStudentCreate} from './templates/PageStudentCreate';
+import {PageRepresentative} from './templates/PageRepresentative';
+import {PageRepresentativeCreate} from './templates/PageRepresentativeCreate';
+
 
 window.addEventListener("load",()=>{
 	render((
@@ -28,6 +33,11 @@ window.addEventListener("load",()=>{
 		    </Route>
 		    <Route path="/student" component={App}>
 		    	<IndexRoute component={PageStudent}/>
+		    	<Route path="create" component={PageStudentCreate}/>
+		    </Route>
+		    <Route path="/representative" component={App}>
+		    	<IndexRoute component={PageRepresentative}/>
+		    	<Route path="create" component={PageRepresentativeCreate}/>
 		    </Route>
 	  	</Router>
 	  ), document.body
