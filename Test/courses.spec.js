acfc7d8df8053fc6e168f4d9cb93217977085944
Jhonny Mata <solucionesmatajm@gmail.com>
2017-04-14 13:42:14 -0400

    correcciones pruebas de materia

--- Test/courses.spec.js
+++ Test/courses.spec.js
@@ -1,57 +1,38 @@
 var utils=require("./utils.js");
 var faker = require('faker');
 
-describe("Test route courses",function(){
-	var self=this,user;
+describe("Test route knowedge cognitions",function(){
+	var self=this,user,find,category;
 	afterEach(utils.dropDatabase.bind(self));
 	beforeEach(function(done){
 		self.db=utils.getDatabase();
-		
-		utils.insertSession(self.db).then(function(data){
-			user=data
+		find=new self.db.schema.Courses({
+			name:faker.name.findName()
+		});		
+		Promise.all([utils.insertSession(self.db), find.save()]).then(function(data){	
+			user=data[0]
 			done();
 		}).catch(function(error){
+			expect(error).toBeNull();
 			done();
 		})
 	})
-
 	
 	it("GET /v1/courses/",function(done){
-		find=new  self.db.schema.Courses({
-			name:faker.name.findName()
-		});
-		find.save().then(function(){
-			return utils.runApp("GET","/v1/courses/");			
-		}).then(function(response){
-			expect(response.length).toBe(1);
+		utils.runApp("GET",`/v1/courses/`).then(function(response){
+			response=JSON.parse(response);
 			expect(response[0].name).toBe(find.name);
 			done();
 		}).catch(function(error){
-			expect(error.toString()).toBeNull();
+			console.log(error);
+			expect(error).toBeNull();
 			done();
 		});
 	});
-	it("POST /v1/courses/",function(done){
-		var name=faker.name.findName().toUpperCase();
-		utils.runApp("POST","/v1/courses/?PublicKeyId="+user.publicKeyId+"&PrivateKeyId="+user.privateKeyId,{
-			form:{
-				name:name
-			}
-		}).then(function(response){
-			expect(response.name).toBe(name);
-			done();
-		}).catch(function(error){
-			expect(error.toString()).toBeNull();
-			done();
-		});
-	});
+	
 	it("GET /v1/courses/:id",function(done){
-		find=new  self.db.schema.Courses({
-			name:faker.name.findName()
-		});
-		find.save().then(function(){
-			return utils.runApp("GET","/v1/courses/"+find._id);
-		}).then(function(response){
+		utils.runApp("GET",`/v1/courses/${find._id}`).then(function(response){
+			response=JSON.parse(response);
 			expect(response.name).toBe(find.name);
 			done();
 		}).catch(function(error){
@@ -59,17 +40,23 @@ describe("Test route courses",function(){
 			done();
 		});	
 	});
+	
+	it("GET /v1/courses/:id (failed)",function(done){
+		utils.runApp("GET",`/v1/courses/00f0f2dd60e8613875e5e488`).then(function(error){
+			expect(error!=undefined).toBe(true);
+			done();
+		}).catch(function(error){
+			expect(error).toBeNull();
+			done();
+		});	
+	});
 	it("PUT /v1/courses/:id",function(done){
-		find=new  self.db.schema.Courses({
-			name:faker.name.findName()
-		});
-		find.save().then(function(){
-			return utils.runApp("PUT","/v1/courses/"+find._id+"?PublicKeyId="+user.publicKeyId+"&PrivateKeyId="+user.privateKeyId,{
+		utils.runApp("PUT",`/v1/courses/${find._id}?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`,{
 			form:{
 				name:"hola"
 			}
-		});
 		}).then(function(response){
+			response=JSON.parse(response);
 			expect(response.name).toBe("HOLA");
 			done();
 		}).catch(function(error){
@@ -77,36 +64,28 @@ describe("Test route courses",function(){
 			done();
 		});	
 	});
-	it("DELETE /v1/courses/:id",function(done){
-		find=new  self.db.schema.Courses({
-			name:faker.name.findName()
-		});
-		find.save().then(function(){
-			return utils.runApp("DEL","/v1/courses/"+find._id+"?PublicKeyId="+user.publicKeyId+"&PrivateKeyId="+user.privateKeyId);
+	it("POST /v1/courses/",function(done){
+		utils.runApp("POST",`/v1/courses/?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`,{
+			form:{
+				name:"1ro",
+			}
 		}).then(function(response){
-			expect(response.name).toBe(find.name);
+			response=JSON.parse(response);			
+			expect(response.name).toBe("1RO");
 			done();
 		}).catch(function(error){
 			expect(error.toString()).toBeNull();
 			done();
 		});	
 	});
-	it("GET /v1/courses/:id bad response",function(done){
-		utils.runApp("GET","/v1/courses/58c9fac8ea59732e59361b12").then(function(response){
-			expect(response.code).toBe('100');
+	it("DELETE /v1/courses/:id",function(done){
+		utils.runApp("DEL",`/v1/courses/${find._id}?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`).then(function(response){
+			response=JSON.parse(response);
+			expect(response._id).toBe(find._id.toString());
 			done();
 		}).catch(function(error){
 			expect(error.toString()).toBeNull();
 			done();
-		});	
-	});
-	it("GET /v1/courses/:id invalid id response",function(done){
-		utils.runApp("GET","/v1/courses/58c9f59732e59361b12").then(function(response){
-			expect(response.code).toBe('130');
-			done();
-		}).catch(function(error){
-			expect(error.toString()).toBeNull();
-			done();
-		});	
+		});
 	});
 })
