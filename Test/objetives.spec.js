d5991155e72b01632d29ca3e52422f43d293d462
Jhonny Mata <solucionesmatajm@gmail.com>
2017-04-14 15:22:43 -0400

    pruebas representantes

--- Test/objetives.spec.js
+++ Test/objetives.spec.js
@@ -1,89 +1,86 @@
+//(pendiente)//
+
 var utils=require("./utils.js");
 var faker = require('faker');
 
 describe("Test route knowedge cognitions",function(){
-	var self=this,user;
+	var self=this,user,find,category;
 	afterEach(utils.dropDatabase.bind(self));
 	beforeEach(function(done){
 		self.db=utils.getDatabase();
-		utils.insertSession(self.db).then(function(data){
-			user=data
+		find=new  self.db.schema.Cognitions({
+			name:faker.name.findName(),
+			description:"mensaje"
+		});
+		category=new self.db.schema.domainsLearning({
+			name:faker.name.findName(),
+			description:"mensaje",
+			cognitions:[find]
+		});
+		Promise.all([utils.insertSession(self.db), category.save()]).then(function(data){	
+			user=data[0]
 			done();
 		}).catch(function(error){
+			expect(error).toBeNull();
 			done();
 		})
 	})
-
-	it("GET /v1/knowedge/:domain/cognitions",function(done){
-		find=new  self.db.schema.domainsLearning({
-			name:faker.name.findName(),
-			cognitions:[self.db.schema.Cognitions({
-				name:faker.name.findName()
-			})]
-		});
-		find.save().then(function(){			
-			return utils.runApp("GET","/v1/knowedge/"+find.name+"/cognitions/");
-		}).then(function(response){
-			expect(response.length).toBe(1);
-			expect(response[0].name).toBe(find.cognitions[0].name);
+	
+	it("GET /v1/learning/domain",function(done){
+		utils.runApp("GET",`/v1/learning/domain/`).then(function(response){
+			response=JSON.parse(response);
+			expect(response[0].name).toBe(category.name);
 			done();
 		}).catch(function(error){
-			expect(error.toString()).toBeNull();
+			console.log(error);
+			expect(error).toBeNull();
 			done();
 		});
 	});
-	it("POST /v1/knowedge/:domain/cognitions",function(done){
-		find=new  self.db.schema.domainsLearning({
-			name:faker.name.findName()			
-		});
-		var name=faker.name.findName().toUpperCase();
-		find.save().then(function(){			
-			return utils.runApp("POST","/v1/knowedge/"+find.name+"/cognitions/?PublicKeyId="+user.publicKeyId+"&PrivateKeyId="+user.privateKeyId,{
-				form:{
-					name:name
-				}
-			});
-		}).then(function(response){
-			expect(response.name).toBe(find.name);
-			expect(response.cognitions[0].name).toBe(name);
-			done();
-		}).catch(function(error){
-			expect(error.toString()).toBeNull();
-			done();
-		});
-	});
-	it("GET /v1/knowedge/:domain/cognitions/:name",function(done){
-		find=new  self.db.schema.domainsLearning({
-			name:faker.name.findName(),
-			cognitions:[self.db.schema.Cognitions({
-				name:faker.name.findName()
-			})]
-		});
-		find.save().then(function(){
-			return utils.runApp("GET","/v1/knowedge/"+find.name+"/cognitions/"+find.cognitions[0].name.toLowerCase());
-		}).then(function(response){
-			expect(response.name).toBe(find.cognitions[0].name);
+	
+	it("GET /v1/knowedge/:domain/cognitions/:id",function(done){
+		utils.runApp("GET",`/v1/learning/domain/${category._id}`).then(function(response){
+			response=JSON.parse(response);
+			expect(response.name).toBe(category.name);
 			done();
 		}).catch(function(error){
 			expect(error.toString()).toBeNull();
 			done();
 		});	
 	});
+	
+	it("GET /v1/knowedge/:domain/cognitions/:id (failed)",function(done){
+		utils.runApp("GET",`/v1/learning/domain/00f0f2dd60e8613875e5e488`).then(function(error){
+			expect(error!=undefined).toBe(true);
+			done();
+		}).catch(function(error){
+			expect(error).toBeNull();
+			done();
+		});	
+	});
 	it("PUT /v1/knowedge/:domain/cognitions/:id",function(done){
-		find=new  self.db.schema.domainsLearning({
-			name:faker.name.findName(),
-			cognitions:[self.db.schema.Cognitions({
-				name:faker.name.findName()
-			})]
-		});
-		find.save().then(function(){
-			return utils.runApp("PUT","/v1/knowedge/"+find.name+"/cognitions/"+find.cognitions[0]._id+"?PublicKeyId="+user.publicKeyId+"&PrivateKeyId="+user.privateKeyId,{
+		utils.runApp("PUT",`/v1/learning/domain/${category._id}?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`,{
 			form:{
 				name:"hola"
 			}
-		});
 		}).then(function(response){
-			expect(response.cognitions[0].name).toBe("HOLA");
+			response=JSON.parse(response);			
+			expect(response.name).toBe("HOLA");
+			done();
+		}).catch(function(error){
+			expect(error.toString()).toBeNull();
+			done();
+		});	
+	});
+	it("POST /v1/knowedge/:domain/cognitions/",function(done){
+		utils.runApp("POST",`/v1/learning/domain/?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`,{
+			form:{
+				name:"hola",
+				description:"proceso de hola"
+			}
+		}).then(function(response){
+			response=JSON.parse(response);	
+			expect(response.name).toBe("HOLA");
 			done();
 		}).catch(function(error){
 			expect(error.toString()).toBeNull();
@@ -91,20 +88,13 @@ describe("Test route knowedge cognitions",function(){
 		});	
 	});
 	it("DELETE /v1/knowedge/:domain/cognitions/:id",function(done){
-		find=new  self.db.schema.domainsLearning({
-			name:faker.name.findName(),
-			cognitions:[self.db.schema.Cognitions({
-				name:faker.name.findName()
-			})]
-		});
-		find.save().then(function(){
-			return utils.runApp("DEL","/v1/knowedge/"+find.name+"/cognitions/"+find.cognitions[0]._id+"?PublicKeyId="+user.publicKeyId+"&PrivateKeyId="+user.privateKeyId);
-		}).then(function(response){
-			expect(response.cognitions.length).toBe(0);
+		utils.runApp("DEL",`/v1/learning/domain/${category._id}?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`).then(function(response){
+			response=JSON.parse(response);
+			expect(response._id).toBe(category._id.toString());
 			done();
 		}).catch(function(error){
 			expect(error.toString()).toBeNull();
 			done();
-		});	
+		});
 	});
-})
+});
--- Test/representantes.spec.js
+++ Test/representantes.spec.js
@@ -1,12 +1,11 @@
 var utils=require("./utils.js");
 var faker = require('faker');
 
-describe("Test route students",function(){
-	var self=this,user;
+describe("Test route knowedge cognitions",function(){
+	var self=this,user,find,category;
 	afterEach(utils.dropDatabase.bind(self));
 	beforeEach(function(done){
 		self.db=utils.getDatabase();
-		var p1=utils.insertSession(self.db);
 		self.people=self.db.schema.Peoples({
 			dni:"V12345679",
 			name:"people",
@@ -22,82 +21,101 @@ describe("Test route students",function(){
 		self.grade=self.db.schema.Grades({
 			name:"1ro"
 		});
-		self.teacher=self.db.schema.Students({
+		self.student=self.db.schema.Students({
 			data:self.people2,
 			grade:self.grade,
 			discapacityLevel:0
 		});
-		Promise.all([self.grade.save(),p1,self.people.save(),self.people2.save(),self.teacher.save()]).then(function(data){
-			user=data[0];
-			self.represent=self.db.schema.Representatives({
-				data:self.people,
-				idStudent:self.teacher._id
-			});
-			return self.represent.save();
-		}).then(function(data){
+		self.parent=self.db.schema.Representatives({
+			data:self.people2,
+			students:[self.student]
+		});
+		Promise.all([utils.insertSession(self.db),self.grade.save(), self.people.save(),self.people2.save(),self.student.save(),self.parent.save()]).then(function(data){	
+			user=data[0]
 			done();
 		}).catch(function(error){
+			expect(error).toBeNull();
 			done();
 		})
 	})
-
-	it("GET /v1/people/students/",function(done){
-		utils.runApp("GET","/v1/people/parent/?PublicKeyId="+user.publicKeyId+"&PrivateKeyId="+user.privateKeyId).then(function(response){
-			expect(response.length).toBe(1);
-			expect(response[0].data.dni).toBe("V12345679");
+	
+	it("GET /v1/people/parents",function(done){
+		utils.runApp("GET",`/v1/people/parents/`).then(function(response){
+			response=JSON.parse(response);
+			expect(response[0].data.name).toBe(self.parent.data.name);
+			expect(response[0].students[0]._id).toBe(self.student._id.toString());
+			done();
+		}).catch(function(error){
+			console.log(error);
+			expect(error).toBeNull();
+			done();
+		});
+	});
+	
+	it("GET /v1/people/parents/:id",function(done){
+		utils.runApp("GET",`/v1/people/parents/${self.parent._id}`).then(function(response){
+			response=JSON.parse(response);
+			expect(response.data.name).toBe(self.parent.data.name);
+			expect(response.students[0]._id).toBe(self.student._id.toString());
+			done();
+		}).catch(function(error){
+			expect(error.toString()).toBeNull();
+			done();
+		});	
+	});
+	
+	it("GET /v1/people/parents/:id (failed)",function(done){
+		utils.runApp("GET",`/v1/people/parents/00f0f2dd60e8613875e5e488`).then(function(error){
+			expect(error!=undefined).toBe(true);
+			done();
+		}).catch(function(error){
+			expect(error).toBeNull();
+			done();
+		});	
+	});
+	it("PUT /v1/people/parents/:id",function(done){
+		utils.runApp("PUT",`/v1/people/parents/${self.parent._id}?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`,{
+			form:{
+				name:"hola"
+			}
+		}).then(function(response){
+			response=JSON.parse(response);			
+			expect(response.data.name).toBe("HOLA");
+			done();
+		}).catch(function(error){
+			expect(error.toString()).toBeNull();
+			done();
+		});	
+	});
+	it("POST /v1/people/parents/",function(done){
+		var input={
+			dni:faker.helpers.replaceSymbolWithNumber("V########"),
+			name:faker.name.findName(),
+			birthdate:"1992-03-15",
+			tel:faker.phone.phoneNumberFormat()
+		};
+		utils.runApp("POST",`/v1/people/parents/?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`,{
+			form:input
+		}).then(function(response){
+			response=JSON.parse(response);
+			expect(response.data.dni).toBe(input.dni.toUpperCase());
+			expect(response.data.name).toBe(input.name.toUpperCase());
+			expect(response.data.tel).toBe(input.tel);
 			done();
 		}).catch(function(error){
 			expect(error.toString()).toBeNull();
 			done();
 		});
 	});
-
-	it("POST /v1/people/students/",function(done){
-		utils.runApp("POST","/v1/people/parent/?PublicKeyId="+user.publicKeyId+"&PrivateKeyId="+user.privateKeyId,{
-			form:{
-				dni:"V12345675",
-				name:"people",
-				birthdate:"1992-03-15",
-				grade:self.grade.name,
-				idStudent:self.teacher.data.dni
-			}
-		}).then(function(response){
-			expect(response.data.dni).toBe("V12345675");
+	it("DELETE /v1/people/parents/:id",function(done){
+		utils.runApp("DEL",`/v1/people/parents/${self.parent._id}?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`).then(function(response){
+			response=JSON.parse(response);
+			expect(response._id).toBe(self.parent._id.toString());
 			done();
 		}).catch(function(error){
 			expect(error.toString()).toBeNull();
 			done();
 		});
 	});
-	it("GET /v1/people/students/:id",function(done){
-		utils.runApp("GET","/v1/people/parent/"+self.represent._id+"?PublicKeyId="+user.publicKeyId+"&PrivateKeyId="+user.privateKeyId).then(function(response){
-			expect(response.data.dni).toBe('V12345679');
-			done();
-		}).catch(function(error){
-			expect(error.toString()).toBeNull();
-			done();
-		});	
-	});
-	it("PUT /v1/people/students/:id",function(done){
-		utils.runApp("PUT","/v1/people/parent/"+self.represent._id+"?PublicKeyId="+user.publicKeyId+"&PrivateKeyId="+user.privateKeyId,{
-			form:{
-				name:"coco"
-			}
-		}).then(function(response){
-			expect(response.data.name).toBe("COCO");
-			done();
-		}).catch(function(error){
-			expect(error.toString()).toBeNull();
-			done();
-		});	
-	});
-	it("DELETE /v1/people/students/:id",function(done){
-		utils.runApp("DEL","/v1/people/parent/"+self.represent._id+"?PublicKeyId="+user.publicKeyId+"&PrivateKeyId="+user.privateKeyId).then(function(response){
-			expect(String(response.data.dni)).toBe(String('V12345679'));
-			done();
-		}).catch(function(error){
-			expect(error.toString()).toBeNull();
-			done();
-		});	
-	});
-})
+});
+
