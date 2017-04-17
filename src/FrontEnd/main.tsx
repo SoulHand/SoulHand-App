7c94176d0940ad2108252880a1df864672d47eac
isamar <irisamarrr168@gmail.com>
2017-04-17 19:11:45 -0400

    agregar al mainrutas de congnitivo

--- src/FrontEnd/main.tsx
+++ src/FrontEnd/main.tsx
@@ -59,6 +59,10 @@ import {ListType} from './templates/type/listype';
 import {TypeCreate} from './templates/type/typecreate';
 import {TypeView} from './templates/type/typeview';
 
+import {CongnitiveFunctions} from './templates/congnitive';
+import {ListCongnitive} from './templates/congnitive/listcongnitive';
+import {CongnitiveCreate} from './templates/congnitive/congnitivecreate';
+import {CongnitiveView} from './templates/congnitive/typeview';
 import {Login} from './templates/login';
 
 /*import {PageRepresentative} from './templates/PageRepresentative';
@@ -122,6 +126,10 @@ window.addEventListener("load",()=>{
 		     <Route path="/knowedge" component={KnowedgeLevel} onEnter={Auth}>
 		    	<IndexRoute component={ListKnowedgeLevel}/>
 		    	<Route path="create" component={KnowedgeLevelCreate}/>
+		    </Route>
+		     <Route path="/congnitive" component={Congnitive} onEnter={Auth}>
+		    	<IndexRoute component={ListCongnitive}/>
+		    	<Route path="create" component={CongnitiveCreate}/>
 		    </Route>
 	  	</Router>
 	  ), document.body
