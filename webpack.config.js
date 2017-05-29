aaad34d5400becafbfe6750fc53a2937e194a32d
Jhonny Mata <solucionesmatajm@gmail.com>
2017-05-29 11:46:26 -0400

    ultimos cambios

--- webpack.config.js
+++ webpack.config.js
@@ -3,56 +3,52 @@ var path = require('path')
 
 module.exports = {
   entry: {
-    app:'./src/FrontEnd/main.tsx',
-    style:'./src/FrontEnd/scss/soulhand.scss'
+    app: './src/FrontEnd/main.tsx',
+    style: './src/FrontEnd/scss/soulhand.scss'
   },
   output: {
-    path: __dirname + '/public/js/',
+    path: path.join(__dirname, '/public/js/'),
     filename: '[name].js'
   },
   resolve: {
     extensions: ['', '.webpack.js', '.web.js', '.ts', '.tsx', '.js'],
-    modulesDirectories: ['src/FrontEnd', 'node_modules'],
+    modulesDirectories: ['src/FrontEnd', 'node_modules']
   },
   // devtool: 'source-map',
   module: {
     loaders: [
-      { test: /\.tsx?$/, loader: "ts-loader" },
-      { test: /\.scss$/, loader: "style-loader!css-loader!sass-loader" },
-      { test: /\.css$/, loader: "style-loader!css-loader" }
+      {test: /\.tsx?$/, loader: 'ts-loader'},
+      {test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader'},
+      {test: /\.css$/, loader: 'style-loader!css-loader'}
     ]
-  },/*
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM",
-    "react-router": "ReactRouter"
-  },*/
+  },
   plugins: [
     new webpack.ProvidePlugin({
-       $: "jquery",
-       validator: "string-validator",
-       jQuery: "jquery",
-       Tether:"tether",
-       React: "react",
-       ReactDOM: "react-dom",
-       ReactRouter: "react-router",
-       'window.settings': path.join(__dirname, 'src/FrontEnd/settings.js')
+      $: 'jquery',
+      validator: 'string-validator',
+      jQuery: 'jquery',
+      Tether: 'tether',
+      React: 'react',
+      ReactDOM: 'react-dom',
+      ReactRouter: 'react-router',
+      'window.settings': path.join(__dirname, 'src/FrontEnd/settings.js')
     }),
-    // new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
-    /*new webpack.optimize.UglifyJsPlugin({
-        compress: {
-            warnings: false
-        }
-    }),*/
     new webpack.DefinePlugin({
-        NODE_ENV: 'development'
-    })/*,
-     new BowerWebpackPlugin({
-      modulesDirectories: ["bower_components"],
-      manifestFiles:      "bower.json",
-      includes:           "/.*\/",
-      excludes:           [],
-      searchResolveModulesDirectories: true
-    })*/
+      NODE_ENV: 'development'
+    })
   ]
 }
+// new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.bundle.js'),
+/*
+new webpack.optimize.UglifyJsPlugin({
+    compress: {
+        warnings: false
+    }
+}),
+ new BowerWebpackPlugin({
+  modulesDirectories: ["bower_components"],
+  manifestFiles:      "bower.json",
+  includes:           "/.*\/",
+  excludes:           [],
+  searchResolveModulesDirectories: true
+}) */
