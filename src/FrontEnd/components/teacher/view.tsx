aadc7b34e060052333a62e50c026df8718271a93
Jhonny Mata <solucionesmatajm@gmail.com>
2017-05-31 10:02:24 -0400

    correcciones de iconos front

--- src/FrontEnd/components/teacher/view.tsx
+++ src/FrontEnd/components/teacher/view.tsx
@@ -52,81 +52,58 @@ import {Teacher} from '../cards/teacher'
        body = (
          <div className="mdl-grid mdl-color--white demo-content">
           <div className="mdl-cell--6-col mdl-cell--middle">
-            <div className="mdl-grid">
-              <div className="mdl-cell--2-col mdl-cell--middle">
-                <i className="material-icons">ic_person</i>
-              </div>
-              <div className="mdl-cell--6-col mdl-cell--middle">
-                <div className="mdl-textfield">
-                  <label className="mdl-input__expandable-holder">Nombre Y Apellido</label>
-                  <div className="mdl-textfield__input">
-                    {this.state.teacher.data.name}
-                  </div>
-                </div>
+            <div className="mdl-textfield">
+              <label className="mdl-input__expandable-holder">Nombre Y Apellido</label>
+              <div className="mdl-textfield__input">
+                {this.state.teacher.data.name}
               </div>
             </div>
           </div>
           <div className="mdl-cell--6-col mdl-cell--middle">
-            <div className="mdl-grid">
-              <div className="mdl-cell--2-col mdl-cell--middle">
-                <i className="material-icons">ic_person_pin</i>
-              </div>
-              <div className="mdl-cell--6-col mdl-cell--middle">
-                <div className="mdl-textfield">
-                  <label className="mdl-input__expandable-holder">Documento de identidad</label>
-                  <div className="mdl-textfield__input">
-                    {this.state.teacher.data.dni}
-                  </div>
-                </div>
+            <div className="mdl-textfield">
+              <label className="mdl-input__expandable-holder">Documento de identidad</label>
+              <div className="mdl-textfield__input">
+                {this.state.teacher.data.dni}
               </div>
             </div>
           </div>
           <div className="mdl-cell--6-col mdl-cell--middle">
-            <div className="mdl-grid">
-              <div className="mdl-cell--2-col mdl-cell--middle">
-                <i className="material-icons">ic_calendar</i>
-              </div>
-              <div className="mdl-cell--6-col mdl-cell--middle">
-                <div className="mdl-textfield">
-                  <label className="mdl-input__expandable-holder">Fecha de nacimiento</label>
-                  <div className="mdl-textfield__input">
-                    {this.state.teacher.data.birthdate}
-                  </div>
-                </div>
+            <div className="mdl-textfield">
+              <label className="mdl-input__expandable-holder">Fecha de nacimiento</label>
+              <div className="mdl-textfield__input">
+                {this.state.teacher.data.birthdate}
               </div>
             </div>
           </div>
           <div className="mdl-cell--6-col mdl-cell--middle">
-            <div className="mdl-grid">
-              <div className="mdl-cell--2-col mdl-cell--middle">
-                <i className="material-icons">ic_calendar</i>
-              </div>
-              <div className="mdl-cell--6-col mdl-cell--middle">
-                <div className="mdl-textfield">
-                  <label className="mdl-input__expandable-holder">Creado en</label>
-                  <div className="mdl-textfield__input">
-                    {this.state.teacher.data.createDate}
-                  </div>
-                </div>
+            <div className="mdl-textfield">
+              <label className="mdl-input__expandable-holder">Creado en</label>
+              <div className="mdl-textfield__input">
+                {this.state.teacher.data.createDate}
               </div>
             </div>
           </div>
           <div className="mdl-cell--6-col mdl-cell--middle">
-            <div className="mdl-grid">
-              <div className="mdl-cell--2-col mdl-cell--middle">
-                <i className="material-icons">ic_calendar</i>
+            <div className="mdl-textfield">
+              <label className="mdl-input__expandable-holder">Grado</label>
+              <div className="mdl-textfield__input">
+                {(this.state.teacher.grade) ? (
+                  this.state.teacher.grade.name
+                ) : (
+                  <span>No asignado</span>
+                )}
               </div>
-              <div className="mdl-cell--6-col mdl-cell--middle">
-                <div className="mdl-textfield">
-                  <label className="mdl-input__expandable-holder">Grado</label>
-                  <div className="mdl-textfield__input">
-                    {(this.state.teacher.grade) ? (
-                      this.state.teacher.grade.name
-                    ) : (
-                      <span>No asignado</span>
-                    )}
-                  </div>
-                </div>
+            </div>
+          </div>
+          <div className="mdl-cell--6-col mdl-cell--middle">
+            <div className="mdl-textfield">
+              <label className="mdl-input__expandable-holder">Interprete</label>
+              <div className="mdl-textfield__input">
+                {(this.state.teacher.interprete==true) ? (
+                    <span>Interprete</span>
+                ) : (
+                  <span>Usuario</span>
+                )}
               </div>
             </div>
           </div>
