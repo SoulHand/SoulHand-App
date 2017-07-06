94990c8872efa73057c52544c5778b6fe457016c
Jhonny Mata <solucionesmatajm@gmail.com>
2017-07-06 10:50:01 -0400

    correciones de eventos e inferencias

--- test/events.inferences.spec.js
+++ test/events.inferences.spec.js
@@ -5,58 +5,54 @@ describe("Test route knowedge cognitions",function(){
 	var self=this,user,find,category;
 	afterEach(utils.dropDatabase.bind(self));
 	beforeEach(function(done){
-		self.db=utils.getDatabase();
-		find=new  self.db.schema.inferences({
+		self.schema=utils.getDatabase();
+		find=new  self.schema.inferences({
 			premise:"p1==\"hola\"",
 			consecuent:"q1=\"saludo\"",
 			h:0.85
 		});
-		category=new self.db.schema.events({
+		category=new self.schema.events({
 			name:faker.name.findName(),
 			objects:{
 				p1:"var1"
 			},
 			premises:[find]
 		});
-		Promise.all([utils.insertSession(self.db), category.save()]).then(function(data){	
+		Promise.all([utils.insertSession(self.schema), category.save()]).then(function(data){
 			user=data[0]
 			done();
 		}).catch(function(error){
-			expect(error).toBeNull();
-			done();
+			done(error);
 		})
 	})
-	
+
 	it("GET /v1/events/type",function(done){
 		utils.runApp("GET",`/v1/events/type`).then(function(response){
 			response=JSON.parse(response);
 			expect(response[0].name).toBe(category.name);
 			done();
 		}).catch(function(error){
-			expect(error).toBeNull();
-			done();
+			done(error);
 		});
 	});
-	
+
 	it("GET /v1/events/type/:id",function(done){
 		utils.runApp("GET",`/v1/events/type/${category._id}`).then(function(response){
-			response=JSON.parse(response);
+			response = JSON.parse(response);
 			expect(response.name).toBe(category.name);
 			done();
 		}).catch(function(error){
-			expect(error.toString()).toBeNull();
-			done();
-		});	
+			done(error);
+		});
 	});
-	
+
 	it("GET /v1/events/type/:id (failed)",function(done){
 		utils.runApp("GET",`/v1/events/type/00f0f2dd60e8613875e5e488`).then(function(error){
 			expect(error!=undefined).toBe(true);
 			done();
 		}).catch(function(error){
-			expect(error).toBeNull();
-			done();
-		});	
+			done(error);
+		});
 	});
 	it("PUT /v1/events/type/:id",function(done){
 		utils.runApp("PUT",`/v1/events/type/${category._id}?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`,{
@@ -64,13 +60,12 @@ describe("Test route knowedge cognitions",function(){
 				name:"hola"
 			}
 		}).then(function(response){
-			response=JSON.parse(response);
+			response = JSON.parse(response);
 			expect(response.name).toBe("HOLA");
 			done();
 		}).catch(function(error){
-			expect(error.toString()).toBeNull();
-			done();
-		});	
+			done(error);
+		});
 	});
 	it("POST /v1/events/type/",function(done){
 		utils.runApp("POST",`/v1/events/type?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`,{
@@ -90,7 +85,7 @@ describe("Test route knowedge cognitions",function(){
 		}).catch(function(error){
 			expect(error.toString()).toBeNull();
 			done();
-		});	
+		});
 	});
 	it("DELETE /v1/events/type/:id",function(done){
 		utils.runApp("DEL",`/v1/events/type/${category._id}?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`).then(function(response){
