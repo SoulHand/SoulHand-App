c41e4c942b94902260bb7fb2da91c803ff3c16be
Jhonny Mata <solucionesmatajm@gmail.com>
2017-07-06 10:46:23 -0400

    correcciones de pruebas cursos

--- test/courses.spec.js
+++ test/courses.spec.js
@@ -5,19 +5,18 @@ describe("Test route knowedge cognitions",function(){
 	var self=this,user,find,category;
 	afterEach(utils.dropDatabase.bind(self));
 	beforeEach(function(done){
-		self.db=utils.getDatabase();
-		find=new self.db.schema.Courses({
+		self.schema = utils.getDatabase();
+		find = new self.schema.Courses({
 			name:faker.name.findName()
-		});		
-		Promise.all([utils.insertSession(self.db), find.save()]).then(function(data){	
-			user=data[0]
+		});
+		Promise.all([utils.insertSession(self.schema), find.save()]).then(function(data){
+			user = data[0]
 			done();
 		}).catch(function(error){
-			expect(error).toBeNull();
-			done();
-		})
+			done(error);
+		});
 	})
-	
+
 	it("GET /v1/courses/",function(done){
 		utils.runApp("GET",`/v1/courses/`).then(function(response){
 			response=JSON.parse(response);
@@ -28,7 +27,7 @@ describe("Test route knowedge cognitions",function(){
 			done();
 		});
 	});
-	
+
 	it("GET /v1/courses/:id",function(done){
 		utils.runApp("GET",`/v1/courses/${find._id}`).then(function(response){
 			response=JSON.parse(response);
@@ -37,9 +36,9 @@ describe("Test route knowedge cognitions",function(){
 		}).catch(function(error){
 			expect(error.toString()).toBeNull();
 			done();
-		});	
+		});
 	});
-	
+
 	it("GET /v1/courses/:id (failed)",function(done){
 		utils.runApp("GET",`/v1/courses/00f0f2dd60e8613875e5e488`).then(function(error){
 			expect(error!=undefined).toBe(true);
@@ -47,7 +46,7 @@ describe("Test route knowedge cognitions",function(){
 		}).catch(function(error){
 			expect(error).toBeNull();
 			done();
-		});	
+		});
 	});
 	it("PUT /v1/courses/:id",function(done){
 		utils.runApp("PUT",`/v1/courses/${find._id}?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`,{
@@ -61,7 +60,7 @@ describe("Test route knowedge cognitions",function(){
 		}).catch(function(error){
 			expect(error.toString()).toBeNull();
 			done();
-		});	
+		});
 	});
 	it("POST /v1/courses/",function(done){
 		utils.runApp("POST",`/v1/courses/?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`,{
@@ -69,13 +68,13 @@ describe("Test route knowedge cognitions",function(){
 				name:"1ro",
 			}
 		}).then(function(response){
-			response=JSON.parse(response);			
+			response=JSON.parse(response);
 			expect(response.name).toBe("1RO");
 			done();
 		}).catch(function(error){
 			expect(error.toString()).toBeNull();
 			done();
-		});	
+		});
 	});
 	it("DELETE /v1/courses/:id",function(done){
 		utils.runApp("DEL",`/v1/courses/${find._id}?PublicKeyId=${user.publicKeyId}&PrivateKeyId=${user.privateKeyId}`).then(function(response){
