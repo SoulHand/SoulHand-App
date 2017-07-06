9577298540687b46937d76bc2855a3d332242069
Jhonny Mata <solucionesmatajm@gmail.com>
2017-07-06 10:43:23 -0400

    correccion de montado de pruebas

--- test/cognitions.spec.js
+++ test/cognitions.spec.js
@@ -5,22 +5,21 @@ describe("Test route knowedge cognitions",function(){
 	var self=this,user,find,category;
 	afterEach(utils.dropDatabase.bind(self));
 	beforeEach(function(done){
-		self.db=utils.getDatabase();
-		find=new  self.db.schema.Cognitions({
+		self.schema=utils.getDatabase();
+		find=new  self.schema.Cognitions({
 			name:faker.name.findName(),
 			description:"mensaje"
 		});
-		category=new self.db.schema.domainsLearning({
+		category=new self.schema.domainsLearning({
 			name:faker.name.findName(),
 			description:"mensaje",
 			cognitions:[find]
 		});
-		Promise.all([utils.insertSession(self.db), category.save()]).then(function(data){
+		Promise.all([utils.insertSession(self.schema), category.save()]).then(function(data){
 			user=data[0]
 			done();
 		}).catch(function(error){
-			expect(error).toBeNull();
-			done();
+			done(error);
 		})
 	});
 	it("GET /v1/knowedge/:domain/cognitions",function(done){
