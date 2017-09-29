6d8b4b317690f1ca540089dc4447d45dba1e87ab
Jhonny Mata <solucionesmatajm@gmail.com>
2017-09-29 18:55:27 -0400

    linea de tiempo progreso actividades completada y asignadas

--- src/FrontEnd/components/dashboard/index.tsx
+++ src/FrontEnd/components/dashboard/index.tsx
@@ -17,12 +17,17 @@ import {LineChart} from "../linechart"
      componentHandler.upgradeAllRegistered();
    }
    componentDidMount(){
-     window.progress.start();
      let p1 = ajax({
        method:"GET",
        url: `${window._BASE}/v1/activities/teacher?PublicKeyId=${this.session.publicKeyId}&PrivateKeyId=${this.session.privateKeyId}`,
        dataType: "json",
-       data:null
+       data:null,
+       beforeSend: () => {
+        window.progress.start();
+       },
+       complete: () => {
+         window.progress.done();
+       }
      });
      p1.done((activities: Array<CRUD.activity>) => {
        let parse = this.parseGraphs(activities);
@@ -30,7 +35,6 @@ import {LineChart} from "../linechart"
          activities: activities,
          values: parse
        });
-       window.progress.done();
      });
    }
    parseGraphs(activities: Array<CRUD.activity>){
@@ -42,7 +46,8 @@ import {LineChart} from "../linechart"
          count: 0,
          progress: 0,
          categories: [],
-         domains: []
+         domains: [],
+         completeds: []
        },
        objetives:{
          completed: 0,
@@ -74,6 +79,31 @@ import {LineChart} from "../linechart"
        } else {
          count.activities.pending++;
        }
+       var isAdd = count.activities.categories.filter((str: string) => {
+         return row.course.name == str;
+       });
+       if (isAdd.length == 0) {
+         var node = {
+           name: row.course.name,
+           data: [0, 0, 0, 0, 0, 0]
+         };
+         count.activities.categories.push(row.course.name);
+         count.activities.domains.push(node);
+         count.activities.completeds.push(node);
+       }
+     });
+     count.activities.domains = count.activities.domains.map((row: { name: string, data: Array<number> }, index: number) => {
+       activities.forEach((objetive) => {
+        var date = new Date(objetive.dateCreated);
+        var dayWeek = date.getDay();
+        if (objetive.course.name == row.name){
+          row.data[dayWeek]++;
+          if (objetive.isCompleted) {
+            count.activities.completeds[index].data[dayWeek]++;
+          }
+        }
+       });
+       return row;
      });
      objetives.forEach((row) => {
        var isAdd = count.objetives.categories.filter((str: string) => {
@@ -104,7 +134,7 @@ import {LineChart} from "../linechart"
      if(!this.state.activities){
        return null;
      }
-     var Colors = ["rgb(24, 146, 77)", "#F7C65F"];
+     var Colors = ["#90ed7d", "rgb(80, 67, 67)"];
       var completeVsPending = {
         credits: false,
         chart: {
@@ -154,87 +184,60 @@ import {LineChart} from "../linechart"
             ]
         }],
       };
-      var Colors = ["#649AE1", "#55BADF", "#F7C65F", "#A992E2", "#EC6F5A", "#48C9A9", "#85D27A"];
-     var DomainObjetives = {
-       credits: false,
-       chart: {
-         type: 'pie',
-       },
-       tooltip: {
-         pointFormat: '{series.name}: <b>{point.y} Actividades</b>'
-       },
-       plotOptions: {
-         pie: {
-           innerSize: '60%',
-           center: ['50%', '50%'],
-           cursor: 'pointer',
-           dataLabels: {
-             enabled: false
-           },
-           showInLegend: true
-         }
-       },
-       colors: Colors,
-       title: {
-         text: `Cobertura de actividades`,
-         align: 'center',
-         style: {
-           fontFamily: "Roboto",
-           fontSize: "1.5em",
-           fontWeight: "bold",
-           color: "#888",
-         }
-       },
-       subtitle: {
-         text: `${this.state.values.objetives.count}`,
-         align: 'center',
-         verticalAlign: 'middle',
-         y: 1,
-         style: {
-           fontFamily: "Roboto",
-           fontSize: "3em",
-           fontWeight: "bold",
-           color: "#888"
-         }
-       },
-       series: [{
-         data: this.state.values.objetives.domains
-       }]
+      var Colors = ["#E19431", "#55BADF", "#90ed7d"];
+      var DomainObjetives = {
+        credits: false,
+        chart: {
+          type: 'pie',
+        },
+        tooltip: {
+          pointFormat: '{series.name}: <b>{point.y} Actividades</b>'
+        },
+        plotOptions: {
+          pie: {
+            innerSize: '60%',
+            center: ['50%', '50%'],
+            cursor: 'pointer',
+            dataLabels: {
+              enabled: false
+            },
+            showInLegend: true
+          }
+        },
+        colors: Colors,
+        title: {
+          text: `Cobertura de actividades`,
+          align: 'center',
+          style: {
+            fontFamily: "Roboto",
+            fontSize: "1.5em",
+            fontWeight: "bold",
+            color: "#888",
+          }
+        },
+        subtitle: {
+          text: `${this.state.values.objetives.count}`,
+          align: 'center',
+          verticalAlign: 'middle',
+          y: 1,
+          style: {
+            fontFamily: "Roboto",
+            fontSize: "3em",
+            fontWeight: "bold",
+            color: "#888"
+          }
+        },
+        series: [{
+          data: this.state.values.objetives.domains
+        }]
       };
-      var series: Array <any> = [];
-      this.state.activities.forEach((activity) => {
-        let add = null;
-        for(var i in series){
-          if(series[i].name === activity.course.name){
-            add = series[i];
-            break;
-          }
-        }
-        if(!add){
-          series.push({
-            name: activity.course.name,
-            data: [0, 0, 0, 0, 0, 0, 0]
-          });
-          add = series[series.length-1]
-        }
-        if(activity.isCompleted == true){
-          var date = new Date(activity.dateCompleted);
-          var days = (Date.now()-date.getTime())/8.64e+7;
-          if(days <= 7){
-            var dayWeek = date.getDay();
-            if(!add.data[dayWeek]){
-              add.data[dayWeek] = 0;
-            }
-            add.data[dayWeek]++;
-          }
-        }
-      })
-     var ObjetivesCompleted = {
+      var ObjetivesCompleted = {
+        credits: false,
         chart: {
             type: 'column'
         },
         title:{
-          text:"Aceptación de objetivos",
+          text:"Actividades asignadas",
           x:-20,
           style: {
             fontFamily: "Roboto",
@@ -248,32 +251,54 @@ import {LineChart} from "../linechart"
             text:"días de la semana"
           }
         },
-        series: series
+        series: this.state.values.activities.domains
       };
-     return(
-       <div className="mdl-grid demo-content">
-         <div className="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
-           <LineChart id="completedvsfailed" config={completeVsPending} className="mdl-cell mdl-cell--8-col mdl-cell--6-col-desktop width-center" autoSize={false} title="El porcentaje de progreso mide el avance semanal de tus alumnos."/>
-           <LineChart id="domainobjetive" config={DomainObjetives} className="mdl-cell mdl-cell--8-col mdl-cell--6-col-desktop width-center" autoSize={false} title="La covertura de actividades mide los objetivos a cumplir."/>
-         </div>
-         <div className="demo-graphs mdl-shadow--2dp mdl-color--white mdl-cell mdl-cell--8-col">
-          <LineChart id="objetivecompleted" config={ObjetivesCompleted}/>
-           <div id="graph1"></div>
-         </div>
-         <div className="demo-cards mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-grid mdl-grid--no-spacing">
-           <div className="demo-updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--12-col-desktop">
-             <div className="mdl-card__title mdl-card--expand mdl-color--teal-300 dog">
-               <h2 className="mdl-card__title-text">Aqui van actualizaciones</h2>
-             </div>
-             <div className="mdl-card__supporting-text mdl-color-text--grey-600">
-               Non dolore elit adipisicing ea reprehenderit consectetur culpa.
-             </div>
-             <div className="mdl-card__actions mdl-card--border">
-               <a href="#" className="mdl-button mdl-js-button mdl-js-ripple-effect">Read More</a>
-             </div>
-           </div>
-         </div>
-       </div>
-     );
+      var ActivitieCompleted = {
+        credits: false,
+        chart: {
+            type: 'column'
+        },
+        title:{
+          text:"Actividades completadas",
+          x:-20,
+          style: {
+            fontFamily: "Roboto",
+            fontWeight: "bold",
+            color: "#888"
+          }
+        },
+        xAxis:{
+          categories:["dom", "Lun","Mart","Mier","Jue","Vier", "sab"],
+          title:{
+            text:"días de la semana"
+          }
+        },
+        series: this.state.values.activities.completeds
+      };
+      return(
+        <div className="mdl-grid demo-content">
+          <div className="mdl-color--white mdl-shadow--2dp mdl-cell mdl-cell--12-col mdl-grid">
+            <LineChart id="completedvsfailed" config={completeVsPending} className="mdl-cell mdl-cell--8-col mdl-cell--6-col-desktop width-center" autoSize={false} title="El porcentaje de progreso mide el avance semanal de tus alumnos."/>
+            <LineChart id="coverageObjetive" config={DomainObjetives} className="mdl-cell mdl-cell--8-col mdl-cell--6-col-desktop width-center" autoSize={false} title="La covertura de actividades mide los objetivos a cumplir."/>
+          </div>
+          <div className="demo-graphs mdl-shadow--2dp mdl-color--white mdl-cell mdl-cell--8-col">
+            <LineChart id="WeekActivities" config={ObjetivesCompleted}/>
+            <LineChart id="ActivitiesCompleted" config={ActivitieCompleted}/>
+          </div>
+          <div className="demo-cards mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet mdl-grid mdl-grid--no-spacing">
+            <div className="demo-updates mdl-card mdl-shadow--2dp mdl-cell mdl-cell--4-col mdl-cell--4-col-tablet mdl-cell--12-col-desktop">
+              <div className="mdl-card__title mdl-card--expand mdl-color--teal-300 dog">
+                <h2 className="mdl-card__title-text">Aqui van actualizaciones</h2>
+              </div>
+              <div className="mdl-card__supporting-text mdl-color-text--grey-600">
+                Non dolore elit adipisicing ea reprehenderit consectetur culpa.
+              </div>
+              <div className="mdl-card__actions mdl-card--border">
+                <a href="#" className="mdl-button mdl-js-button mdl-js-ripple-effect">Read More</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
    }
  }
