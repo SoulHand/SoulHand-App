6e4d3cd364d60faf4af7bfc3fec7f6bbf8a4e870
Jhonny Mata <solucionesmatajm@gmail.com>
2017-10-12 07:22:55 -0400

    eliminando enlaces de lexema y información semantica

--- src/FrontEnd/components/app/menu.tsx
+++ src/FrontEnd/components/app/menu.tsx
@@ -27,9 +27,7 @@ import {UserBox} from './userbox'
             <div className="mdl-layout-spacer"></div>
             <Link to="/matters" className="mdl-navigation__link"><i className="mdl-color-text--blue-grey-400 material-icons" role="presentation">school</i>Materias</Link>
             <Link to="/grades" className="mdl-navigation__link"><i className="mdl-color-text--blue-grey-400 material-icons" role="presentation">recent_actors</i>Grados</Link>
-            <Link to="/words" className="mdl-navigation__link"><i className="mdl-color-text--blue-grey-400 material-icons" role="presentation">recent_actors</i>Lexemas</Link>
             <Link to="/words/words" className="mdl-navigation__link"><i className="mdl-color-text--blue-grey-400 material-icons" role="presentation">recent_actors</i>Palabras</Link>
-            <Link to="/terms" className="mdl-navigation__link"><i className="mdl-color-text--blue-grey-400 material-icons" role="presentation">recent_actors</i>Inf. semantica</Link>
           </nav>
        </div>
      );
