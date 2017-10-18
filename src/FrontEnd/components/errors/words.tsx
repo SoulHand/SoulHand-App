c59e2cbea4ba308caba942f1172e239a130f4061
Jhonny Mata <solucionesmatajm@gmail.com>
2017-10-18 09:29:28 -0400

    registro de morfemas y lexemas asistente de palabras

--- src/FrontEnd/components/errors/words.tsx
+++ src/FrontEnd/components/errors/words.tsx
@@ -90,6 +90,9 @@ export class Words extends FormUtils<Props.GenericRouter, {}>{
         isPending: !!data[2]
       };
   }
+  replace_word(data: Words.word){
+    this.setState({word: data});
+  }
   new_word_step1(e: React.EventHandler<any>){
     var _words = document.querySelectorAll("input[data-field]:checked");
     var keywords = [];
@@ -110,7 +113,7 @@ export class Words extends FormUtils<Props.GenericRouter, {}>{
     this.now = this.data.shift();
     this.props.router.replace(`/errors/2/words/new`);
   }
-  new_word_step2(e: React.EventHandler<any>){
+  new_word_step2(e: any){
     var is_valid: any = document.getElementById("correct-lex");
     if(is_valid.checked){
       this.props.router.replace(`/errors/4/words/new`);
@@ -216,7 +219,7 @@ export class Words extends FormUtils<Props.GenericRouter, {}>{
  }
 
 
- class WordStep2 extends React.Component<{load: Function, word: string, next: Function, session: User.session}, {word: Words.word, concepts: Array<Words.Term>, isPending:boolean}>{
+ class WordStep2 extends React.Component<{load: Function, replace: Function, word: string, next: Function, session: User.session}, {word: Words.word, concepts: Array<Words.Term>, isPending:boolean}>{
    public init: boolean = false;
    componentDidMount() {
      let p1 = ajax({
@@ -233,7 +236,6 @@ export class Words extends FormUtils<Props.GenericRouter, {}>{
       });
       p1.done((activity: any) => {
         this.init = true;
-        console.log(activity, this.props.load);
         this.props.load(activity);
         this.setState({
           word: activity[0],
@@ -246,9 +248,17 @@ export class Words extends FormUtils<Props.GenericRouter, {}>{
     componentDidUpdate(){
       componentHandler.upgradeAllRegistered();
     }
+    finish(data: {lexema: string, morphems: Array<Words.word>}){
+      console.log(data);
+    }
     render(){
       if (!this.init) {
         return (<ModalFree />);
+      }
+      if(!this.state.word){
+        return (
+          <WordNotLexem session={this.props.session} word={this.props.word} next={this.finish}/>
+        );
       }
       return (
         <ModalFree title="Informe de errores">
@@ -295,9 +305,9 @@ export class Words extends FormUtils<Props.GenericRouter, {}>{
     }
  }
 
-class Form extends FormUtils<{ word: string, next: Function, session: User.session, lexema: string, morphems: Array<string>}, {error: compat.Map, word?: Words.word, concepts?: Array<Words.Term>, isPending?: boolean}>{
+class Form extends FormUtils<{ word: string, next: Function, session: User.session, lexema?: any, morphems?: Array<string>}, {error: compat.Map, word?: Words.word, concepts?: Array<Words.Term>, isPending?: boolean, lexema?: Words.Lexema, morphem?: Words.Morphema, term?: Words.Term}>{
    public init: boolean = false;
-   state: {error: compat.Map, word?: Words.word, concepts?: Array<Words.Term>, isPending?: boolean} = {
+   state: { error: compat.Map, word?: Words.word, concepts?: Array<Words.Term>, isPending?: boolean, lexema?: Words.Lexema, morphem?: Words.Morphema, term?: Words.Term } = {
       error:{
         error: null,
         words: []
@@ -308,7 +318,7 @@ class Form extends FormUtils<{ word: string, next: Function, session: User.sessi
     }
  }
 
- class WordStep3 extends Form{
+class WordStep3 extends Form{
    public fields:compat.Map={
       name:{
         value:this.props.word,
@@ -515,3 +525,379 @@ class Form extends FormUtils<{ word: string, next: Function, session: User.sessi
       );
     }
  }
+
+
+class WordNotLexem extends Form{
+  public lexems: Array<Words.Lexema> = [];
+  public lexem: Words.Lexema;
+   public fields:compat.Map={
+      name:{
+        value:this.props.word,
+        required:true
+      }
+    };
+    public concept: any = {
+      key: "",
+      value: ""
+    };
+    loadMorphem(data: Array<Words.Morphema>){
+      this.props.next({
+        lexem: this.lexem._id,
+        morphems: data
+      });
+    }
+    send(event: any){
+      var values: compat.Map = {};
+      var error = false;
+      var _button = event.target;
+      for(let i in this.fields){
+        this.state.error[i] = !super.validate(this.fields[i].value, i);
+        values[i] = this.fields[i].value;
+        error = error || this.state.error[i];
+      }
+      if (error) {
+        this.setState(this.state);
+        return;
+      }
+      for(let i = 0, n = this.lexems.length; i < n; i++){
+        if(this.lexems[i].key == values.name.toUpperCase()){
+          this.lexem = this.lexems[i];
+          return this.setState({ lexema: this.lexems[i]});
+        }
+      }
+      ajax({
+          method:"POST",
+          url: `${window._BASE}/v1/words/lexemas/?PublicKeyId=${this.session.publicKeyId}&PrivateKeyId=${this.session.privateKeyId}`,
+          dataType: "json",
+          data:values,
+          beforeSend: () => {
+            window.progress.start();
+            _button.disabled = true;
+          },
+          complete: () => {
+            window.progress.done();
+            _button.removeAttribute("disabled");
+          },
+          success:(data:Words.Lexema)=>{
+            this.lexem = data;
+            this.setState({lexema: data});
+          },
+          error: (data: any) => {
+            var state: CRUD.codeError = data.responseJSON;
+            var config = {
+              message: state.message,
+              timeout: window.settings.alert.delay
+            };
+            var message: any = document.querySelector('.mdl-js-snackbar')
+            message.MaterialSnackbar.showSnackbar(config);
+          }
+      });
+    }
+    componentDidMount() {
+     let p1 = ajax({
+       method: "GET",
+       url: `${window._BASE}/v1/words/lexemas?PublicKeyId=${this.session.publicKeyId}&PrivateKeyId=${this.session.privateKeyId}`,
+       dataType: "json",
+       data: null,
+       beforeSend: () => {
+         window.progress.start();
+       },
+       complete: () => {
+         window.progress.done();
+       }
+      });
+      p1.done((activity: Array<Words.Lexema>) => {
+        this.lexems = activity;
+        this.init = true;
+        this.forceUpdate();
+      });
+      componentHandler.upgradeAllRegistered();
+    }
+    addKey(e: React.EventHandler<any>): any {
+      if (this.concept.key.trim() == "") {
+        this.state.error.term = true;
+        this.setState({ error: this.state.error });
+        return null;
+      }
+      this.fields.term.value.push(this.concept.key);
+      this.fields.term.labels.push(this.concept.value);
+      this.concept = {
+        key: "",
+        value: ""
+      };
+      this.forceUpdate();
+    }
+    componentDidUpdate(){
+      componentHandler.upgradeAllRegistered();
+    }
+    deleteItem(id: number) {
+      this.fields.term.value.splice(id, 1);
+      this.fields.term.labels.splice(id, 1);
+      this.forceUpdate();
+    }
+    getConcept(e: any){
+      this.concept.key = e.target.value;
+      this.concept.value = e.target.selectedOptions[0].label;
+      this.state.error.words = false;
+      this.setState({ error: this.state.error });
+    }
+    render(){
+      if (!this.init) {
+        return (<ModalFree />);
+      }
+      if (this.state.lexema){
+        return (
+          <WordNotMorphem session={this.session} word={this.props.word} lexema={this.state.lexema} next={this.loadMorphem.bind(this)}/>
+        );
+      }
+      return (
+        <ModalFree title="Informe de errores">
+          <div className="mdl-grid demo-content">
+            <div className="mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--9-col mdl-grid">
+              <div className="mdl-cell mdl-cell--11-col">
+                <h3>{this.props.word}</h3>
+                <p>Escriba el lexema (palabra raíz) correspondiente a la palabra <b>{this.props.word}</b></p>
+              </div>
+              <div className="mdl-cell mdl-cell--10-col">
+                <div className={"mdl-textfield mdl-js-textfield mdl-textfield--floating-label " + ((this.state.error.name) ? 'is-invalid' : '')}>
+                  <input type="text" className="mdl-textfield__input" id="name" onChange={(e: any) => { this.getFields(e) }} />
+                  <label className="mdl-textfield__label" htmlFor="name">Lexema*</label>
+                  <span className="mdl-textfield__error">Es necesaria una definición</span>
+                </div>
+              </div>
+              <div className="mdl-cell mdl-cell--11-col mdl-grid">
+                <div className="mdl-cell mdl-cell--9-col">
+                </div>
+                <div className="mdl-cell mdl-cell--2-col">
+                  <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onClick={this.send.bind(this)}>
+                    Siguiente
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </ModalFree>
+      );
+    }
+}
+
+class WordNotMorphem extends Form{
+  public lexem: string;
+  public morphems: Array<string> = [];
+  public morphem: string;
+  public fields: compat.Map = {
+    name: {
+      match: (fn: string) => {
+        return !validator.isNull()(fn);
+      },
+      value: null,
+      required: true
+    },
+    concepts: {
+      match: (fn: string) => {
+        return fn.length > 0;
+      },
+      value: [],
+      labels: [],
+      required: true
+    },
+    regexp: {
+      value: null
+    }
+  };
+  public concept: any = {
+    key: "",
+    value: ""
+  };
+  constructor(props: any){
+    super(props);
+    this.morphems = props.word.trim().split(props.lexema.key);
+    while (!this.morphem){
+      this.morphem = this.morphems.shift().trim();
+    }
+    this.fields.name.value = this.morphem;
+  }
+  send(event: any) {
+    var values: compat.Map = {};
+    var error = false;
+    var _button = event.target;
+    for (var i in this.fields) {
+      this.state.error[i] = !super.validate(this.fields[i].value, i);
+      values[i] = this.fields[i].value;
+      error = error || this.state.error[i];
+    }
+    this.setState(this.state);
+    if (error) {
+      return;
+    }
+    values.concepts = JSON.stringify(values.concepts);
+    ajax({
+      method: "POST",
+      url: `${window._BASE}/v1/words/lexemas/${this.props.lexema._id}/morphemas/?PublicKeyId=${this.session.publicKeyId}&PrivateKeyId=${this.session.privateKeyId}`,
+      dataType: "json",
+      data: values,
+      beforeSend: () => {
+        window.progress.start();
+        _button.disabled = true;
+      },
+      complete: () => {
+        window.progress.done();
+        _button.removeAttribute("disabled");
+      },
+      success: (data: Words.Lexema) => {
+        this.props.next(data);
+      },
+      error: (data: any) => {
+        var state: CRUD.codeError = data.responseJSON;
+        var config = {
+          message: state.message,
+          timeout: window.settings.alert.delay
+        };
+        var message: any = document.querySelector('.mdl-js-snackbar')
+        message.MaterialSnackbar.showSnackbar(config);
+      }
+    });
+  }
+  addKey(e: React.EventHandler<any>): any {
+    if (this.concept.key.trim() == "") {
+      this.state.error.term = true;
+      this.setState({ error: this.state.error });
+      return null;
+    }
+    this.fields.concepts.value.push({
+      key: this.concept.key,
+      value: this.concept.value
+    });
+    this.fields.concepts.labels.push(this.concept.value);
+    this.concept = {
+      key: "",
+      value: ""
+    };
+    this.forceUpdate();
+  }
+  componentDidUpdate(){
+    componentHandler.upgradeAllRegistered();
+  }
+  deleteItem(id: number) {
+    this.fields.concepts.value.splice(id, 1);
+    this.fields.concepts.labels.splice(id, 1);
+    this.forceUpdate();
+  }
+  getConcept1(e: any){
+    this.concept.key = e.target.value;
+    this.state.error.words = false;
+    this.setState({ error: this.state.error });
+  }
+  getConcept2(e: any){
+    this.concept.value = e.target.value;
+    this.state.error.words = false;
+    this.setState({ error: this.state.error });
+  }
+  loadMorphem(data: Words.Morphema) {
+    //this.morphems.push(data);
+  }
+  render(){
+    var keys = null;
+    switch (this.concept.key) {
+      case window.WORDS.CONCEPTS.TIME:
+        keys = Object.keys(window.WORDS.TIMES).map((row) => {
+          var MORPHES: any = window.WORDS.TIMES;
+          return (
+            <option key={row} value={MORPHES[row]}>{MORPHES[row]}</option>
+          );
+        })
+        break;
+      case window.WORDS.CONCEPTS.COUNT:
+        keys = Object.keys(window.WORDS.COUNT).map((row) => {
+          var MORPHES: any = window.WORDS.COUNT;
+          return (
+            <option key={row} value={MORPHES[row]}>{MORPHES[row]}</option>
+          );
+        })
+        break;
+      case window.WORDS.CONCEPTS.GENERO:
+        keys = Object.keys(window.WORDS.GENERO).map((row) => {
+          var MORPHES: any = window.WORDS.GENERO;
+          return (
+            <option key={row} value={MORPHES[row]}>{MORPHES[row]}</option>
+          );
+        })
+        break;
+      case window.WORDS.CONCEPTS.CLASS:
+        keys = Object.keys(window.WORDS.CLASS_GRAMATICAL).map((row) => {
+          var MORPHES: any = window.WORDS.CLASS_GRAMATICAL;
+          return (
+            <option key={row} value={MORPHES[row]}>{MORPHES[row]}</option>
+          );
+        })
+        break;
+    }
+    return (
+      <ModalFree title="Informe de errores">
+        <div className="mdl-grid demo-content">
+          <div className="mdl-color--white mdl-shadow--4dp content mdl-color-text--grey-800 mdl-cell mdl-cell--9-col mdl-grid">
+            <div className="mdl-cell mdl-cell--11-col">
+              <h3>{this.morphem}</h3>
+              <p>Seleccione las funciones gramaticales correspondientes a la palabra <b>{this.props.word}</b></p>
+            </div>
+            <div className="mdl-cell--12-col mdl-cell--middle">
+              <p><b>Morfema:</b> {this.morphem}</p>
+            </div>
+            <div className="mdl-cell--12-col mdl-cell--middle">
+              <div className="mdl-grid">
+                <div className="mdl-cell--5-col mdl-cell--middle" style={{ marginRight: "5px" }}>
+                  <div className={"mdl-textfield mdl-textfield " + ((this.state.error.key || this.state.error.concepts) ? 'is-invalid' : '')}>
+                    <label className="label static" htmlFor="key">Tipo de concepto</label>
+                    <select className="mdl-textfield__input" id="key" onChange={this.getConcept1.bind(this)} value={this.concept.key}>
+                      <option value="">Seleccione una opción</option>
+                      {Object.keys(window.WORDS.CONCEPTS).map((row) => {
+                        var MORPHES: any = window.WORDS.CONCEPTS;
+                        return (
+                          <option key={row} value={MORPHES[row]}>{MORPHES[row]}</option>
+                        );
+                      })}
+                    </select>
+                    <span className="mdl-textfield__error">Es necesario un concepto gramátical</span>
+                  </div>
+                </div>
+                <div className="mdl-cell--5-col mdl-cell--middle mdl-cell--3-col-phone" style={{ marginRight: "5px" }}>
+                  <div className={"mdl-textfield mdl-textfield " + ((this.state.error.value) ? 'is-invalid' : '')}>
+                    <label className="label static" htmlFor="value">Valor</label>
+                    <select className="mdl-textfield__input" id="value" onChange={this.getConcept2.bind(this)} value={this.concept.value}>
+                      <option value="">Seleccione una opción</option>
+                      {keys}
+                    </select>
+                    <span className="mdl-textfield__error">Es necesario un concepto gramátical</span>
+                  </div>
+                </div>
+                <div className="mdl-cell--1-col mdl-cell--middle">
+                  <button id="add-keyword" className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored" onClick={this.addKey.bind(this)}>
+                    <i className="material-icons">add</i>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <div className="mdl-cell--11-col mdl-cell--middle">
+              {this.fields.concepts.value.map((row: any, index: number) => {
+                return (
+                  <span className="mdl-chip" key={index}>
+                    <span className="mdl-chip__text">{this.fields.concepts.labels[index]}</span>
+                    <button type="button" className="mdl-chip__action" onClick={this.deleteItem.bind(this, index)}><i className="material-icons">cancel</i></button>
+                  </span>
+                );
+              })}
+            </div>
+            <div className="mdl-cell mdl-cell--11-col mdl-grid">
+              <div className="mdl-cell mdl-cell--9-col"/>
+              <div className="mdl-cell mdl-cell--2-col">
+                <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent" onClick={this.send.bind(this)}>
+                  Siguiente
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ModalFree>
+    );
+  }
+}
